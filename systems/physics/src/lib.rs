aa19de4a5e5678c99fcd502b9005e402268bf791
Brooks Townsend <brooksmtownsend@gmail.com>
2019-09-21 13:24:07 -0400

    Respond to decs registry pings, publish new positions (#2)
    * Add new_position function to calculate position given old position, velocity and elapsed time. Change components from f32 to f64 representations.
    
    * Respond to decs system registry pings, publish messages for new positions, require "entity" value in frame
    
    * Resolve comments on PR

--- systems/physics/src/lib.rs
+++ systems/physics/src/lib.rs
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+extern crate decscloud_codec as codec;
 extern crate serde_json;
 extern crate wascap_guest as guest;
 
+use codec::systemmgr::System;
 use guest::prelude::*;
 use serde_json::Value;
 use stacktrader_types as trader;
@@ -22,42 +24,109 @@ use trader::components::*;
 
 call_handler!(handle_call);
 
+const NO_MESSAGE: &str = "(no message)";
+const REGISTRY_SUBJECT: &str = "decs.system.registry";
+const POSITION: &str = "position";
+const VELOCITY: &str = "velocity";
+
 pub fn handle_call(ctx: &CapabilitiesContext, operation: &str, msg: &[u8]) -> CallResult {
     match operation {
-        messaging::OP_DELIVER_MESSAGE => handle_frame(ctx, msg),
+        messaging::OP_DELIVER_MESSAGE => handle_message(ctx, msg),
         core::OP_HEALTH_REQUEST => Ok(vec![]),
         _ => Err("bad dispatch".into()),
     }
 }
 
-/// Receives messages on the subject `systems.{shard}.{system}.frames`, e.g. `systems.default.physics.frames`
-fn handle_frame(
+/// Routes message either to the `handle_ping` function for registry pings or `handle_frame` for position updates
+fn handle_message(
     ctx: &CapabilitiesContext,
     msg: impl Into<messaging::DeliverMessage>,
 ) -> CallResult {
     let msg = msg.into().message;
+    let subject = msg
+        .as_ref()
+        .map_or(NO_MESSAGE.to_string(), |m| m.subject.to_string());
     ctx.log(&format!(
         "Received message from broker on subject '{}'",
-        msg.as_ref()
-            .map_or("(no message)".to_string(), |m| m.subject.to_string())
+        subject
     ));
-    let bodyvec = msg.unwrap().body;
+    match subject.as_ref() {
+        NO_MESSAGE => Err("No message".into()),
+        REGISTRY_SUBJECT => handle_ping(ctx, msg.unwrap()),
+        _ => handle_frame(ctx, msg.unwrap()),
+    }
+}
 
-    // This frame should have three objects: "elapsed", "position" and "velocity"
-    let (_elapsed, _pos, _vel) = extract_frame(&bodyvec)?;
+/// Receives messages on the subject `system.registry` and replies with physics system metadata
+fn handle_ping(
+    ctx: &CapabilitiesContext,
+    msg: guest::prelude::messaging::BrokerMessage,
+) -> CallResult {
+    let payload = System {
+        name: "physics".to_string(),
+        framerate: 1,
+        components: vec![POSITION.to_string(), VELOCITY.to_string()],
+    };
+    let ref reply_to = match msg.reply_to.len() {
+        0 => format!("{}.replies", REGISTRY_SUBJECT),
+        _ => msg.reply_to,
+    };
+    if let Err(_) = ctx
+        .msg()
+        .publish(reply_to, None, &serde_json::to_vec(&payload)?)
+    {
+        return Err("Error publishing message".into());
+    };
+    Ok(vec![])
+}
 
+/// Receives messages on the subject `decs.systems.{shard}.{system}.frames`, e.g. `decs.systems.default.physics.frames`
+fn handle_frame(
+    ctx: &CapabilitiesContext,
+    msg: guest::prelude::messaging::BrokerMessage,
+) -> CallResult {
+    let subject: Vec<&str> = msg.subject.split(".").collect();
+    if subject.len() != 5 {
+        return Err("Unknown message subject received".into());
+    }
+
+    match extract_frame(&msg.body) {
+        Ok(v) => {
+            let (entity, elapsed, pos, vel) = v;
+            if vel.mag == 0 {
+                return Ok(vec![]);
+            } else if vel.ux == 0.0 && vel.uy == 0.0 && vel.uz == 0.0 {
+                return Err("Bad target vector".into());
+            }
+
+            if let Ok(new_position) = new_position(elapsed, &pos, &vel) {
+                let publish_subject = &format!("decs.{}.{}.position.put", subject[2], entity);
+                if let Err(_) = ctx.msg().publish(
+                    publish_subject,
+                    None,
+                    &serde_json::to_vec(&new_position)?,
+                ) {
+                    return Err("Error publishing message".into());
+                };
+            };
+        }
+        Err(_) => {
+            return Err("Did not receive components needed for frame update".into());
+        }        
+    };
     Ok(vec![])
 }
 
 /// Extracts the elapsed time, position and velocity values out of the aggregate frame
-fn extract_frame(frame_raw: &[u8]) -> Result<(u64, Position, Velocity)> {
+fn extract_frame(frame_raw: &[u8]) -> Result<(String, u64, Position, Velocity)> {
     let v: Value = serde_json::from_slice(frame_raw)?;
 
+    let entity = serde_json::from_value(v["entity"].clone())?;
     let elapsed = serde_json::from_value(v["elapsed"].clone())?;
     let pos = serde_json::from_value(v["position"].clone())?;
     let vel = serde_json::from_value(v["velocity"].clone())?;
 
-    Ok((elapsed, pos, vel))
+    Ok((entity, elapsed, pos, vel))
 }
 
 /// Calculates a new position based on a current position and velocity over an elapsed time
@@ -79,6 +148,7 @@ mod test {
     fn test_extract_frame() {
         let data = br#"
         {
+            "entity": "34a79797-163e-474b-a8ff-970f2808c1b1",
             "elapsed": 16,
             "position": {
                 "x": 1,
@@ -94,7 +164,8 @@ mod test {
         }
         "#;
 
-        let (elapsed, pos, vel) = extract_frame(data).unwrap();
+        let (entity, elapsed, pos, vel) = extract_frame(data).unwrap();
+        assert_eq!(entity, "34a79797-163e-474b-a8ff-970f2808c1b1".to_string());
         assert_eq!(elapsed, 16);
         assert_eq!(pos.x, 1.0);
         assert_eq!(pos.y, 2.5);
@@ -105,6 +176,7 @@ mod test {
     fn test_new_position() {
         let data = br#"
         {
+            "entity": "34a79797-163e-474b-a8ff-970f2808c1b1",
             "elapsed": 16,
             "position": {
                 "x": 1,
@@ -120,7 +192,8 @@ mod test {
         }
         "#;
 
-        let (elapsed, pos, vel) = extract_frame(data).unwrap();
+        let (entity, elapsed, pos, vel) = extract_frame(data).unwrap();
+        assert_eq!(entity, "34a79797-163e-474b-a8ff-970f2808c1b1".to_string());
         assert_eq!(elapsed, 16);
         assert_eq!(pos.x, 1.0);
         assert_eq!(pos.y, 30.0);
@@ -137,4 +210,38 @@ mod test {
         assert_eq!(new_pos_two.y, 30.064);
         assert_eq!(new_pos_two.z, -9.936);
     }
+
+    #[test]
+    fn test_no_position_change() {
+        let data = br#"
+        {
+            "entity": "34a79797-163e-474b-a8ff-970f2808c1b1",
+            "elapsed": 16,
+            "position": {
+                "x": 11.0,
+                "y": -49.0,
+                "z": 20.0
+            },
+            "velocity": {
+                "mag": 0,
+                "ux": 1.0,
+                "uy": 1.0,
+                "uz": 1.0
+            }
+        }
+        "#;
+        let (entity, elapsed, pos, vel) = extract_frame(data).unwrap();
+        assert_eq!(entity, "34a79797-163e-474b-a8ff-970f2808c1b1".to_string());
+        assert_eq!(elapsed, 16);
+        assert_eq!(pos.x, 11.0);
+        assert_eq!(pos.y, -49.0);
+        assert_eq!(pos.z, 20.0);
+        assert_eq!(vel.mag, 0);
+
+        let new_pos = new_position(elapsed, &pos, &vel).unwrap();
+        assert_eq!(new_pos.x, pos.x);
+        assert_eq!(new_pos.y, pos.y);
+        assert_eq!(new_pos.z, pos.z);
+    }
+
 }
